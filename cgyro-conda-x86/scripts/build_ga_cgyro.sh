f640c498494115bd633ba59c882eae7fd5052dc4
Igor Sfiligoi <isfiligoi@ucsd.edu>
2025-03-04 16:51:23 -0800

    Add cgyro-conda-x86

---
+++ cgyro-conda-x86/scripts/build_ga_cgyro.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+conda activate gacode
+export GACODE_ROOT=/opt/gacode
+export GACODE_PLATFORM=CONDA_OMPI_GNU
+. $GACODE_ROOT/platform/env/env.$GACODE_PLATFORM
+. $GACODE_ROOT/shared/bin/gacode_setup
+
+cd /opt/gacode && cd cgyro && make && cd ../xgyro && make
---
+++ cgyro-conda-x86/scripts/install_ga_git.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+mkdir -p /opt && cd /opt && git clone https://github.com/gafusion/gacode.git
