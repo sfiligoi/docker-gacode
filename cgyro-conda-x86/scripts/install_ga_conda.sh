18b9d7892c6581151317794f9db52cc69e3ae751
Igor Sfiligoi <isfiligoi@ucsd.edu>
2025-03-04 17:01:09 -0800

    Remove aggressive clean

--- cgyro-conda-x86/scripts/install_ga_conda.sh
+++ cgyro-conda-x86/scripts/install_ga_conda.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 conda create -y -n gacode -c conda-forge gfortran binutils make python fftw openmpi netcdf4 libopenblas=*=*openmp* vim git
-conda clean -y --all
+conda clean -y --tarballs 
