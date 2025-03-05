f32dcef13e9bc11cdbc5b93699482f1f5a638df5
Igor Sfiligoi <isfiligoi@ucsd.edu>
2025-03-04 17:32:51 -0800

    Add safe.directory

--- cgyro-conda-x86/scripts/install_ga_git.sh
+++ cgyro-conda-x86/scripts/install_ga_git.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-mkdir -p /opt && cd /opt && git clone https://github.com/gafusion/gacode.git
+mkdir -p /opt && cd /opt && git clone https://github.com/gafusion/gacode.git && git config --global --add safe.directory /opt/gacode
